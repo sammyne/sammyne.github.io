c286587b817c5c5147ef5e7ca5147d9e8c405de0
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-04-08 13:57:14 +0800

    feature(home): add the footer

--- layouts/home.vue
+++ layouts/home.vue
@@ -6,6 +6,9 @@
         <nuxt/>
       </v-container>
     </v-content>
+    <v-footer class="pa-3">
+      <v-layout justify-center>&copy; 2019 - Sammy</v-layout>
+    </v-footer>
   </v-app>
 </template>
 
