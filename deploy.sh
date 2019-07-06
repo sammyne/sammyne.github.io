aabd7701662d659212c7428836b3e614a7d61b84
Sammy <lixiangmin01@baidu.com>
2019-07-06 17:36:25 +0800

    test(ci): testing another way of push

--- deploy.sh
+++ deploy.sh
@@ -17,11 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-#git push -f git@github.com:sammyne/sammyne.github.io.git master
-
-# for CircleCI
-git remote add origin https://github.com/sammyne/sammyne.github.io.git
-git push -f origin master
+git push -f git@github.com:sammyne/sammyne.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
