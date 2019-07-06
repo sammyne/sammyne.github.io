e805f4e042b2841f2a5456dc9f4841245bd38c32
Sammy <lixiangmin01@baidu.com>
2019-07-06 16:06:14 +0800

    feature: introduce the circle-CI

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,8 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:sammyne/sammyne.github.io.git master
+#git push -f git@github.com:sammyne/sammyne.github.io.git master
+git push -f origin master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
