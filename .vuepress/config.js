8099cd5a4b827b5f5b74f60a936cec4fc995db2e
sammyne <lixiangmin01@baidu.com>
2020-09-14 09:30:30 +0800

    style(vuepress): localize navbar based on Chinese

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -17,7 +17,7 @@ module.exports = {
     themeConfig: {
         nav: [
             { text: 'Home', link: '/', icon: 'reco-home' },
-            { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
+            { text: '时间轴', link: '/timeline/', icon: 'reco-date' },
             { text: 'Github', link: 'https://github.com/sammyne', icon: 'reco-github' },
             //{
             //    text: 'Contact',
@@ -33,11 +33,11 @@ module.exports = {
         blogConfig: {
             category: {
                 location: 2, // 在导航栏菜单中所占的位置，默认2
-                text: 'Category' // 默认 “分类”
+                text: '分类' // 默认 “分类”
             },
             tag: {
                 location: 3, // 在导航栏菜单中所占的位置，默认3
-                text: 'Tag' // 默认 “标签”
+                text: '标签' // 默认 “标签”
             }
         },
         friendLink: [
