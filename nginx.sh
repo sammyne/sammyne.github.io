8aac557cdda2cd8cdcd421c87b8fc5113ce8cc61
sammyne <lixiangmin01@baidu.com>
2021-04-18 10:34:59 +0800

    fix(ci): adjust github actions based on the new layout

--- nginx.sh
+++ nginx.sh
@@ -2,7 +2,9 @@
 
 echo "pre-deploy @localhost:9090"
 
+webappDir=$PWD/_post
+
 docker run --rm \
-    -v ${PWD}/.vuepress/dist:/usr/share/nginx/html \
+    -v ${webappDir}/.vuepress/dist:/usr/share/nginx/html \
     --name html -p 9090:80 \
     nginx:1.19.6-alpine
